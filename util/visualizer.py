7822b9c623e5b5f94b043833d91183984497e620
donghaoye <donghaoye@qq.com>
2017-09-18 23:03:58 +0800

    fix bug when no use visdom

--- util/visualizer.py
+++ util/visualizer.py
@@ -60,15 +60,16 @@ class Visualizer():
             self.plot_data = {'X':[],'Y':[], 'legend':list(errors.keys())}
         self.plot_data['X'].append(epoch + counter_ratio)
         self.plot_data['Y'].append([errors[k] for k in self.plot_data['legend']])
-        self.vis.line(
-            X=np.stack([np.array(self.plot_data['X'])]*len(self.plot_data['legend']),1),
-            Y=np.array(self.plot_data['Y']),
-            opts={
-                'title': self.name + ' loss over time',
-                'legend': self.plot_data['legend'],
-                'xlabel': 'epoch',
-                'ylabel': 'loss'},
-            win=self.display_id)
+        if self.display_id > 0:
+            self.vis.line(
+                X=np.stack([np.array(self.plot_data['X'])]*len(self.plot_data['legend']),1),
+                Y=np.array(self.plot_data['Y']),
+                opts={
+                    'title': self.name + ' loss over time',
+                    'legend': self.plot_data['legend'],
+                    'xlabel': 'epoch',
+                    'ylabel': 'loss'},
+                win=self.display_id)
 
     # errors: same format as |errors| of plotCurrentErrors
     def print_current_errors(self, epoch, i, errors, t):
